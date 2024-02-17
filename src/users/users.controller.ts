fcf28e95557be96083fde5c3f0f0ee85bdb3b018
amenibensaada <amenisaada19@gmail.com>
2024-02-17 05:40:48 -1100

    implementing user registration

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,25 +8,25 @@ import {
   Delete,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { User } from './users.schema';
 import { CreateUserInput } from './dto/createUserDto';
+import { UserWithoutPassword } from './users.repository';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  async findAll(): Promise<User[]> {
+  async findAll(): Promise<UserWithoutPassword[]> {
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<User> {
+  async findOne(@Param('id') id: string): Promise<UserWithoutPassword> {
     return this.usersService.findOne(id);
   }
 
   @Post()
-  async create(@Body() createUserDto: CreateUserInput): Promise<User> {
+  async create(@Body() createUserDto: CreateUserInput) {
     return this.usersService.create(createUserDto);
   }
 
@@ -34,12 +34,12 @@ export class UsersController {
   async update(
     @Param('id') id: string,
     @Body() updateUserDto: CreateUserInput,
-  ): Promise<User> {
+  ): Promise<UserWithoutPassword> {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  async delete(@Param('id') id: string): Promise<User> {
+  async delete(@Param('id') id: string) {
     return this.usersService.delete(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UserRepository } from './users.repository';
-import { User } from './users.schema';
+import { CreateUserInput } from './dto/createUserDto';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
@@ -9,19 +10,32 @@ export class UsersService {
   async findAll() {
     return this.userRepository.findAll();
   }
-  async findOne(id: string): Promise<User> {
+  async findOne(id: string) {
     return this.userRepository.findOne(id);
   }
 
-  async create(createUserDto: any): Promise<User> {
-    return this.userRepository.create(createUserDto);
+  async create(createUserDto: CreateUserInput) {
+    const { password, confirmPassword, firstName, lastName, ...rest } =
+      createUserDto;
+    if (password !== confirmPassword) {
+      throw new HttpException("Passwords don't match", HttpStatus.UNAUTHORIZED);
+    }
+    const hashedPassword = await hash(password, 0);
+    const user = await this.userRepository.create({
+      password: hashedPassword,
+      firstName: firstName,
+      lastName: lastName,
+      ...rest,
+    });
+    console.log('userrrr', user);
+    return user;
   }
 
-  async update(id: string, updateUserDto: any): Promise<User> {
+  async update(id: string, updateUserDto: any) {
     return this.userRepository.update(id, updateUserDto);
   }
 
-  async delete(id: string): Promise<User> {
+  async delete(id: string) {
     return this.userRepository.delete(id);
   }
 }
