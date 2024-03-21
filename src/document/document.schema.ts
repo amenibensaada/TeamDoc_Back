79914ec84fb0f6f67607d6089775e93b3382f310
Hana-Romdhani <romdhani.hana@esprit.tn>
2024-03-21 15:45:22 +0100

    --

--- src/document/document.schema.ts
+++ src/document/document.schema.ts
@@ -2,6 +2,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Schema as MongooseSchema } from 'mongoose';
 import { Folder } from 'src/folder/folder.schema';
 import { User } from 'src/users/users.schema';
+import { boolean } from 'zod';
 
 export type DocumentsDocument = Documents & Document;
 
@@ -22,12 +23,19 @@ export class Documents {
     type: [{ type: MongooseSchema.Types.ObjectId, ref: 'Folder' }]
   })
   folders: Folder;
-  
+
   @Prop({
     type: [{ type: MongooseSchema.Types.ObjectId, ref: 'User' }]
   })
 
   User: User;
+
+  @Prop({ type: boolean, default: false })
+  deleted: boolean;
+  @Prop({ type: Date, default: null })
+
+  deletedAt: Date = null;
+
 }
 
 export const DocumentsSchema = SchemaFactory.createForClass(Documents);
